5f6122a1335ab74efeb73f23edeae85ef27b4a73
Katy <k.gaythorpe@imperial.ac.uk>
2021-10-05 10:08:46 +0100

    adding a quick plot

--- B-visualisations.R
+++ B-visualisations.R
@@ -1,2 +1,19 @@
 library(dplyr)
 library(ggplot2)
+
+df <- read.csv("cleaned_data.csv",
+               stringsAsFactors = FALSE)
+
+#quick plot of studies by year
+df %>%
+  group_by(year_of_article) %>%
+  summarise(count = n()) %>%
+  
+  ggplot()+
+  aes(x = year_of_article) +
+  geom_histogram(fill = "lightgreen")+
+  theme_minimal()+
+  labs(x = "Year of article",
+       y =  "Article count")
+
+ggsave("figures/studies_by_year.png", height = 10, width = 12)
