978ded35dd282e1ee344a93f58ee5669fb0aad58
Katy <k.gaythorpe@imperial.ac.uk>
2022-02-15 10:07:13 +0000

    updating map with multi country studies

--- B-visualisations.R
+++ B-visualisations.R
@@ -9,6 +9,9 @@ library(ggrepel)
 df <- read.csv("cleaned_data.csv",
                stringsAsFactors = FALSE)
 
+#read additional tab for multi country studies
+df_multi <- readxl::read_xlsx("data/Data extraction vaccine equity.xlsx", sheet = "Multi country studies")
+
 #================================================================================================================
 #quick plot of studies by year
 df %>%
@@ -17,13 +20,13 @@ df %>%
   mutate(is_complete = ifelse(year_of_article==2021, FALSE, TRUE)) %>%
   
   ggplot()+
-  aes(x = year_of_article, y=count, fill = count, alpha = is_complete) +
-  geom_col()+
+  aes(x = year_of_article, y=count,  alpha = is_complete) +
+  geom_col(fill = viridis::magma(9)[5])+
   theme_minimal()+
   labs(x = "Year of article",
-       y =  "Article count")+
-  scale_fill_viridis_c(option = "cividis", end = 0.9)+
-  theme(legend.position = "none")+
+       y =  "Study count")+
+ # scale_fill_viridis_c(option = "magma", end = 0.8)+
+  theme(legend.position = "none", text = element_text(size = 18))+
   scale_alpha_discrete(range = c(0.3, 1))
 
 ggsave("figures/studies_by_year.png", height = 10, width = 12)
@@ -54,6 +57,8 @@ world_map <- world_map %>% mutate(region = toupper(region))
 
 # join 'em up
 world_map <- world_map %>% left_join(df, by = c("region" = "Country"))
+
+
 # add the continents
 world_map <- world_map %>% mutate(continent = countrycode::countrycode(sourcevar = region,
                                                                        origin = "country.name",
@@ -64,17 +69,29 @@ world_map <- world_map %>%
   mutate(study_count = length(na.omit(unique(covidence_id)))) %>%
   ungroup() %>%
   group_by(continent) %>%
-  mutate(continent_study_count = length(na.omit(unique(covidence_id))))
+  mutate(continent_study_count = length(na.omit(unique(covidence_id)))) %>%
+  mutate(iso3 = countrycode(sourcevar = region, origin = "country.name", destination = "iso3c"))
+
+# add multi country
+df_multi$study_count_multi <- rowSums(df_multi[, 2:26]==TRUE, na.rm = TRUE) 
+
+world_map <- world_map %>%
+  left_join(df_multi %>% select(iso3, study_count_multi)) %>%
+  mutate(study_count = ifelse(is.na(study_count), 0, study_count)) %>%
+  mutate(tot_count = ifelse(study_count + study_count_multi>0, study_count+study_count_multi, NA))
+
 
 g_world <- map_bg +
   
   geom_polygon(data = world_map %>%
                   filter(!is.na(region)),
-               aes(x=long, y = lat, group = group, fill = as.factor(study_count)))+ 
+               aes(x=long, y = lat, group = group, fill =(tot_count)))+ 
   
   labs( fill = "Study count") +
-  scale_fill_manual(values = c("azure2",viridis::cividis(7)))+
-  theme(legend.position = "bottom")
+  scale_fill_viridis_c(option = "magma", na.value = "azure2", limits = c(1,max(world_map$tot_count, na.rm = TRUE)), end = 0.8)+
+  #scale_fill_manual(values = c("azure2",viridis::magma(16)))+
+  theme(legend.position = "bottom", text = element_text(size = 18))
+
 
 g_world
 
